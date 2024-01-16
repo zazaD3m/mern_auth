43539dde368e7d6f9d04d1a689547ce27880c65e
zazaD3m <zaza.d3m@gmail.com>
2024-01-16 23:26:32 +0400

    v8

--- frontend/src/screens/GoogleLogin.jsx
+++ frontend/src/screens/GoogleLogin.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom"
 const GoogleLogin = () => {
   return (
     <div>
-      <Link to="/api/users/auth/google">SIGN IN GOOGLE</Link>
+      <Link to="https://mern-auth-7wan.onrender.com/api/users/auth/google">
+        SIGN IN GOOGLE
+      </Link>
     </div>
   )
 }
